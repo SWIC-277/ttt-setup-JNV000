6f6a37105ffbcc3ff3984e73afbfbaea327dd5d6
Jacob Vielweber <jnvielweber@gmail.com>
2022-10-23 00:01:04 -0500

    Code and pass test for square component

---
+++ src/components/Square/Square.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Square from "./Square";
+
+it("renders a square with the correct marker ('X' or 'O')", () => {
+  render(<Square marker="X" />);
+
+  const square = screen.getByRole("button", { name: "X" });
+
+  expect(square).toBeInTheDocument();
+});
