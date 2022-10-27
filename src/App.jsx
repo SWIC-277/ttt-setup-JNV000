8fedaa8c6fd9177cdd765b7f9e661686fc8100e7
Jacob Vielweber <jnvielweber@gmail.com>
2022-10-26 20:11:08 -0500

    render 9 squares in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
 import "./App.css";
 import { useReducer } from "react";
+import Square from "components/Square/Square";
+// configuration file is set up so ./ is not needed for files in src
 
+/*
 function reducer(state, action) {
   switch (action.type) {
     case "made_move": {
+
     }
   }
 }
+*/
 
 function App() {
   // TODO: Add useReducer hook to track state of: board, turn, winner
+  /* video code
   const [state, dispatch] = useReducer(reducer, {
     board: Array[9].fill[""],
     turn: "X",
     winner: null,
   });
+  */
+  const board = new Array(9).fill("");
 
-  return <h1>Tic Tac Toe</h1>;
+  // return <h1>Tic Tac Toe</h1>;
+  return board.map((square, index) => (
+    <Square key={index} id={index} handleClick={() => {}} />
+  ));
 }
 
 export default App;
