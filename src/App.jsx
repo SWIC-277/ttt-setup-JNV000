768eb1dcdc3ab8a1e2d4ea2ec90effbbf92c2964
Jacob Vielweber <jnvielweber@gmail.com>
2022-10-26 20:47:59 -0500

    code app and useGame in class

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
-import "./App.css";
-import { useReducer } from "react";
 import Square from "components/Square/Square";
+import useGame from "./Hooks/useGame";
+import "./App.css";
+
 // configuration file is set up so ./ is not needed for files in src
-
-/*
-function reducer(state, action) {
-  switch (action.type) {
-    case "made_move": {
-
-    }
-  }
-}
-*/
 
 function App() {
   // TODO: Add useReducer hook to track state of: board, turn, winner
-  /* video code
-  const [state, dispatch] = useReducer(reducer, {
-    board: Array[9].fill[""],
-    turn: "X",
-    winner: null,
-  });
-  */
-  const board = new Array(9).fill("");
+  // const board = new Array(9).fill("");
+  const { board, makeMove, winner, turn } = useGame();
 
   // return <h1>Tic Tac Toe</h1>;
   return board.map((square, index) => (
-    <Square key={index} id={index} handleClick={() => {}} />
+    <Square key={index} id={index} handleClick={makeMove} />
   ));
 }
 
---
+++ src/Hooks/useGame.js
@@ -0,0 +1,45 @@
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "made_move": {
+      const board2Update = [...state.board];
+      // TODO: update the board with the curren tturn using the action.index
+      if (board2Update[action.index] == null) {
+        board2Update[action.index] = state.turn;
+      }
+      return {
+        ...state,
+        board: board2Update,
+        turn: state.turn === "X" ? "O" : "X", // if turn was X make O, else X
+      };
+    }
+    case "declared_winner": {
+      return {
+        ...state,
+        winner: action.winner,
+      };
+    }
+    default:
+      throw new Error("Invalid action");
+  }
+}
+
+export default function useGame() {
+  const [state, dispatch] = useReducer(reducer, {
+    board: Array(9).fill(null),
+    turn: "X",
+  });
+
+  const makeMove = (index) => {
+    dispatch({ type: "made_move", index });
+  };
+}
+
+/* video code
+  const [state, dispatch] = useReducer(reducer, {
+    board: Array[9].fill[""],
+    turn: "X",
+    winner: null,
+  });
+  */
