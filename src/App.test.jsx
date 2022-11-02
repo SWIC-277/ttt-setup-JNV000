caa6e416e51f02160af5dad614149af2d10d01c1
Jacob Vielweber <jnvielweber@gmail.com>
2022-11-01 22:19:23 -0500

    Add tests for App

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,37 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-test("renders Tic Tac Toe", () => {
-  render(<App />);
+describe("App", () => {
+  it("renders Tic Tac Toe", () => {
+    render(<App />);
 
-  const squares = screen.getAllByRole("button");
+    const squares = screen.getAllByRole("button");
 
-  expect(squares).toHaveLength(9); // we expect 9 squares on a board
+    expect(squares).toHaveLength(9); // we expect 9 squares on a board
+  });
+
+  it("alternates 'X' and 'O' whenever clicking squares", async () => {
+    // create mock user
+    const user = userEvent.setup();
+
+    render(<App />);
+
+    // get all squares on the 'screen'
+    const squares = screen.getAllByRole("button");
+
+    // 'click' 3 squares
+    await user.click(squares[0]);
+    await user.click(squares[1]);
+    await user.click(squares[2]);
+
+    // squares should alternate between 'X' and 'O'
+    expect(squares[0]).toHaveTextContent("X");
+    expect(squares[1]).toHaveTextContent("O");
+    expect(squares[2]).toHaveTextContent("X");
+  });
+
+  // TODO: Add test for winner
 });
+
+// test("renders Tic Tac Toe", () => {
