fd266c1270c821a9b637e2ff3a9247afeec671fb
Jacob Vielweber <jnvielweber@gmail.com>
2022-11-01 21:00:41 -0500

    Move reducer to its own dedicated file.

---
+++ src/Hooks/reducer.js
@@ -0,0 +1,24 @@
+import checkWinner from "services/game.service";
+
+export default function reducer(state, action) {
+  switch (action.type) {
+    case "made_move": {
+      // only mutate a local copy of the state
+      const board2Update = [...state.board]; // copy the current board state
+
+      if (!board2Update[action.index]) {
+        // if there is nothing in the square we can add the players symbol
+        board2Update[action.index] = state.turn;
+      }
+      return {
+        board: board2Update,
+        turn: state.turn === "X" ? "O" : "X", // if turn was X make O, else X
+        winner: checkWinner(board2Update, state.turn) ? state.turn : null,
+      };
+      // unfortunately as currently coded turn will switch if an already
+      // filled square is clicked and no new marker is drawn.
+    }
+    default:
+      throw new Error("Invalid action");
+  }
+}
--- src/Hooks/useGame.js
+++ src/Hooks/useGame.js
@@ -1,28 +1,5 @@
 import { useReducer } from "react";
-import checkWinner from "services/game.service";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "made_move": {
-      // only mutate a local copy of the state
-      const board2Update = [...state.board]; // copy the current board state
-
-      if (!board2Update[action.index]) {
-        // if there is nothing in the square we can add the players symbol
-        board2Update[action.index] = state.turn;
-      }
-      return {
-        board: board2Update,
-        turn: state.turn === "X" ? "O" : "X", // if turn was X make O, else X
-        winner: checkWinner(board2Update, state.turn) ? state.turn : null,
-      };
-      // unfortunately as currently coded turn will switch if an already
-      // filled square is clicked and no new marker is drawn.
-    }
-    default:
-      throw new Error("Invalid action");
-  }
-}
+import reducer from "./reducer";
 
 export default function useGame() {
   // start with an empty board, on turn X, and no winner
